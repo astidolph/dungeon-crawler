ef32b599434a5189fa2b3e8ba98ae5266eeeddd6
Adam Stidolph <a.stidolph@gmail.com>
2022-12-09 13:29:29 +0000

    Specify a grid layout to organise the game board

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import Deck from './features/Deck/Deck';
 import { drawCard, selectDeck } from './features/Deck/DeckSlice';
 import Hand from './features/Hand/Hand';
 import { gainCoins, selectCoins, selectDamage, selectHealth, selectItems, selectMaxHealth } from './features/PlayerSlice';
-import { CardType } from './models/Card';
+import { Card, CardType } from './models/Card';
 
 function App() {
   const dispatch = useAppDispatch();
@@ -18,6 +18,8 @@ function App() {
   const treasureDeck = useAppSelector((state) => selectDeck(state, CardType.Treasure));
   const items = useAppSelector(selectItems);
 
+  const placeholderMonsterCards: Card[] = [{name: 'fake-monster', id: 0, type: CardType.Loot, effects: []}];
+
   useEffect(() => {
     dispatch(drawCard(CardType.Loot));
     dispatch(drawCard(CardType.Loot));
@@ -27,13 +29,31 @@ function App() {
   
   return (
     <div className="App">
-      <Deck title="Loot" cards={lootDeck} type={CardType.Loot}></Deck>
-      <Deck title="Treasure" cards={treasureDeck} type={CardType.Treasure}></Deck>
-      Coins: {coins}
-      Health: {health}/{maxHealth}
-      Damage: {damage}
-      Items: {items.map(card => <CardComponent card={card}></CardComponent>)}
-      <Hand></Hand>
+      <div className="GameTitleContainer"></div>
+      <div className="TreasureDeckContainer">
+        <Deck title="Treasure Deck" cards={treasureDeck} type={CardType.Treasure}></Deck>
+      </div>
+      <div className="MonsterDeckContainer">
+        <Deck title="Monster Deck" cards={placeholderMonsterCards} type={CardType.Loot}></Deck>
+      </div>
+      <div className="GameBoardContainer"></div>
+      <div className="LootDeckContainer">
+        <Deck title="Loot Deck" cards={lootDeck} type={CardType.Loot}></Deck>
+      </div>
+      <div className="ItemContainer">
+        {items.map(card => <CardComponent card={card}></CardComponent>)}
+      </div>
+      <div className="PlayerStatsContainer">
+        Coins: {coins}
+        Health: {health}/{maxHealth}
+        Damage: {damage}
+      </div>
+      <div className="HandContainer">
+        <Hand></Hand>
+      </div>
+      <div className="EndTurnContainer">
+        End Turn
+      </div>
     </div>
   );
 }
